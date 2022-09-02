1d88b6cf7b12ee00bd1786d479adfa197ab02034
CHUKWUEMEKA EDEH <73679039+EMMYDONE765@users.noreply.github.com>
2022-09-02 05:06:25 +0100

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,17 @@
+#DO NOT DELETE ANY OF THE ALREADY WRITTEN CODES!!!
+# ADD YOURS TO GIVE THE DESIRED OUTPUT
+
+# 1. Write a python state that prints "Hello World"
+
+# 2. Modify the code to print each statement after the fullstop on a newline
+
+print("Python is like any other sport.\n" "You might know the rules, but you have to play to learn.")
+
+# 3. Modify the code below to begin the second sentence with a tab indentation.
+print("Although practicality beats purity.\t" "Errors should never pass silently. Unless explicitly silenced.")
+# 4. Modify the code below to put the paragraph on a single line with a 'hyphen (-)' seperator.
+
+# e.g Explicit is better than implicit. - Simple is better than complex. - Complex is better than complicated.
+
+print('Explicit is better than implicit.' ' -Simple is better than complex.' ' -Complex is better than complicated.')
+
